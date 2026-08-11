83dcd39cb225ebad1b6b942372a21fcf875f5b2c
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-11 12:08:26 +0530

    Update RightAnglePattern.java

--- Patterns/RightAnglePattern.java
+++ Patterns/RightAnglePattern.java
@@ -1,17 +1,26 @@
 package com;
-
+//This program prints a right angle triangle patern of stars upon providing an interger value.
 public class RightAnglePattern {
 
+	//method
 	public void PrintRightAngleTriangle(int n) {
-		for (int i = 0; i<n; i++) {
-			for(int j = 0; j<=i; j++) {
-				System.out.print("*");
+		for (int i = 0; i<n; i++) {               //outer loop determining each line (i = line number).
+			for(int j = 0; j<=i; j++) {           //inner loop - In each line, No. of stars = No. of the line (2nd line = 2 stars) hence, j<=i where i = line number.
+				System.out.print("*");            //prinitng stars.
 			}
-			System.out.println();
+			System.out.println();                //next line
 		}
 	}
 	public static void main(String[] args) {
-		RightAnglePattern RAP = new RightAnglePattern();
-		RAP.PrintRightAngleTriangle(10);
+		RightAnglePattern RAP = new RightAnglePattern();  //object-creation
+		RAP.PrintRightAngleTriangle(4);                  //method called
 	}
 }
+
+/*
+Output
+*
+**
+***
+****
+*/
