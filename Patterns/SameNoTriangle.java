1648efb88eb08e31789e5c1af63d9c02a7b92a78
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-11 12:22:09 +0530

    Update SameNoTriangle.java

--- Patterns/SameNoTriangle.java
+++ Patterns/SameNoTriangle.java
@@ -1,21 +1,28 @@
 package com;
 
 public class SameNoTriangle {
-
+//In this program we have to print a numbered right angle triangle but each line should have repetition of its line number only.
 		public void PrintSameNumberTriangle(int n) {
 			
-			for(int i = 1; i<=n; i++) {
-				for(int j = 1; j<=i; j++) {
-					System.out.print(i);
+			for(int i = 1; i<=n; i++) {  //outer loop: necessary to start from 1 otherwise the first iteration will not occur. It determines line number.
+				for(int j = 1; j<=i; j++) {  //Inner loop: allowed to print only until the line number(i). It iterates 1 time if i =1 and two times if i =2.
+					System.out.print(i);   //We are printing value of i(line number) in each iteration of inner loop, resulting in printing of same number.
 				}
-				System.out.println();
+				System.out.println(); //next line.
 			}
 			
 		}
 		public static void main(String[] args) {
-			SameNoTriangle NT = new SameNoTriangle();
-			NT.PrintSameNumberTriangle(4);
+			SameNoTriangle NT = new SameNoTriangle();  //object creation
+			NT.PrintSameNumberTriangle(4);  //method called
 		}
 	}
 
 
+/*
+Output
+1
+22
+333
+4444
+*/
