cf20c9dfd3222632d54f9fe3c7e04eeae1dddcc1
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-11 12:00:14 +0530

    Update StarPattern.java

--- Patterns/StarPattern.java
+++ Patterns/StarPattern.java
@@ -1,20 +1,20 @@
 package com;
 
 import java.util.Scanner;
-
+//This program takes a number from user as input and prints a n*n star pattern.
 public class StarPattern {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter a number to print the pattern");
-		int N = scan.nextInt();
+		System.out.println("Enter a number to print the pattern");  //prompt
+		int N = scan.nextInt();  //storing input
 		
-		for(int i = 0; i<N; i++) {
-			for(int j = 0; j<N; j++) {
-				System.out.print("*");
+		for(int i = 0; i<N; i++) {  //outer loop - determines number of lines.
+			for(int j = 0; j<N; j++) {  //Inner loop - prints each line.
+				System.out.print("*");  //print statement(same line)
 			}
-			System.out.println();
+			System.out.println();   //empty println statement to shift to next line.
 		}
-		scan.close();
+		scan.close();   //closing taking inputs.
 	}
 }
