799532de6a991980763d4ea888080bebef110d41
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-11 12:22:25 +0530

    Update NumberTriangle.java

--- Patterns/NumberTriangle.java
+++ Patterns/NumberTriangle.java
@@ -4,7 +4,7 @@ public class NumberTriangle {
 //This program has a method that prints a right angle triangle of numbers starting from 1 in each line.
 	public void PrintNumberTriangle(int n) {
 		
-		for(int i = 1; i<=n; i++) {      //outer loop: i =1 as line should start from 1. 
+		for(int i = 1; i<=n; i++) {      //outer loop: necessary to start from 1 otherwise the first iteration will not occur. It determines line number. 
 			for(int j = 1; j<=i; j++) {  //Inner loop: j = 1 as each line should start from 1 and in each line we have to go until the line no.'i' (3rd line: 1 2 3).
 				System.out.print(j);     //In each line we have to print value of j starting from 1.
 			}
