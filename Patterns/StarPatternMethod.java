ae33772f96a3cd133c961523a1d956969b71f9a7
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-11 12:03:36 +0530

    Update StarPatternMethod.java

--- Patterns/StarPatternMethod.java
+++ Patterns/StarPatternMethod.java
@@ -1,19 +1,28 @@
 package com;
-
+//This program has a method which prints a n*n star pattern upon calling with an integer argument.
 public class StarPatternMethod {
-
+	//method
 	public void PrintStarPattern(int n) {
-		for(int i = 0; i<n; i++) {
-			for(int j = 0; j<n; j++) {
+		for(int i = 0; i<n; i++) {        //outer loop determine no. of lines
+			for(int j = 0; j<n; j++) {    //inner loop prints lines
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println();  //shifting to next line.
 		}
 	}
-	
+
+	//main method
 	public static void main(String[] args) {
 		
-		StarPatternMethod SPM = new StarPatternMethod();
-		SPM.PrintStarPattern(4);
+		StarPatternMethod SPM = new StarPatternMethod();   //object creation
+		SPM.PrintStarPattern(4);                           //method calling with argument.
 	}
 }
+
+/*
+Output
+****
+****
+****
+****
+*/
