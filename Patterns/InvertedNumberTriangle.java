c95d9132760ea80b9a8f54623a05f8d2cdf8dc01
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-11 17:51:09 +0530

    Update InvertedNumberTriangle.java

--- Patterns/InvertedNumberTriangle.java
+++ Patterns/InvertedNumberTriangle.java
@@ -3,11 +3,11 @@ package com;
 public class InvertedNumberTriangle {
 
 	void PrintUprightTriangle(int n) {
-		for(int i = 0; i<n; i++) {
-			for(int j = 1; j<(n-i)+1; j++) {
-				System.out.print(j);
+		for(int i = 0; i<n; i++) {           //outer loop determines number of lines of triangle.
+			for(int j = 1; j<=(n-i); j++) {  //inner loop print lines: It should run for a total of n-i times 
+				System.out.print(j);         //printing value of j
 			}
-			System.out.println();
+			System.out.println();  //new line.
 			
 		}
 	}
@@ -17,3 +17,13 @@ public class InvertedNumberTriangle {
 		UNT.PrintUprightTriangle(5);
 	}
 }
+
+/*
+
+12345 i=0  j <= (n-i) ex: 5-0 = 5. The loop will run for 5 times and with the increase in value of i this iteration will decrease.
+1234  i=1  j <= (n-i) ex: 5-1 = 4. The loop will run for 4 times
+123   i=2  j <= (n-i) ex: 5-2 = 3. The loop will run for 3 times
+12    i=3  j <= (n-i) ex: 5-3 = 2. The loop will run for 2 times
+1     i=4  j <= (n-i) ex: 5-4 = 1. The loop will run for 1 times
+
+*/
