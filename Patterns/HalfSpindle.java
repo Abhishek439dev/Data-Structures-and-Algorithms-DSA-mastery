6ebf292b42c8cc28c7c21def624d4da3df0e4f76
Abhishek Saxena <saxenaabhishek439@gmail.com>
2026-08-13 23:55:03 +0530

    Update HalfSpindle.java

--- Patterns/HalfSpindle.java
+++ Patterns/HalfSpindle.java
@@ -4,27 +4,27 @@ public class HalfSpindle {
 //To print half spindle we can print half upright right traingle and then half inverted rihht angle triangle.
 	
 	void PrintTriangle(int n) {
-		//Printing half upright triangle 
-		for (int i =0; i<=n; i++) { 
-			for(int j =0; j<=i; j++) {
-				System.out.print("*");
+		//Printing half upright righnt angle triangle 
+		for (int i =0; i<=n; i++) {        //Outer loop
+			for(int j =0; j<=i; j++) {     //Inner loop
+				System.out.print("*");     //Printing star
 			}
-			System.out.println();
+			System.out.println();          //new line
 		}
 
-		//Printing half inverted triangle.
-		for(int i =0; i<n; i++) {
-			for(int j = i; j<n; j++) {
-				System.out.print("*");
+		//Printing half inverted right angle triangle.
+		for(int i =0; i<n; i++) {        //Outer loop
+			for(int j = i; j<n; j++) {   //Inner loop
+				System.out.print("*");   //Printing star
 			}
-			System.out.println();
+			System.out.println();        //new line
 		}
 	}
 	
 	public static void main(String[] args) {
-		HalfSpindle HS = new HalfSpindle();
-		HS.PrintTriangle(4);
-	}
+		HalfSpindle HS = new HalfSpindle();      //Object creation
+		HS.PrintTriangle(4);                   //Method called.
+	} 
 
 }
 
